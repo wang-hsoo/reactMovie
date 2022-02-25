64d60a4938a00adf13c893edf5db76f3b61fd260
wang-hsoo <dhkd4873@naver.com>
2022-02-25 17:16:57 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
+import Home from "./routes/Home";
+
+import Detail from "./routes/Detail";
 
 function App() {
 
 
-    return null;
+    return (
+      <Router>
+        <Routes>
+          <Route path="/movie" element = {<Detail />} />
+          <Route path="/" element = {<Home />} />
+          
+        </Routes>
+      </Router>
+    );
  
 
   }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,14 @@
 import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
+
 
 function Movie({medium_cover_image, title, genres, summary}){
     return (
         <div>
               <img src={medium_cover_image} />
-              <h2>{title}</h2>
+              <h2>
+                 <Link to ="/movie">{title}</Link>
+              </h2>
               <p>{summary}</p>
               <ul>
                 {genres.map((g) => (
