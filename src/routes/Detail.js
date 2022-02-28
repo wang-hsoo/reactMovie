e33906fadbe54003c6e955d8a27ebe18c33ee1c2
wang-hsoo <dhkd4873@naver.com>
2022-02-28 14:21:31 +0900

    commit

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,44 @@
 import {useParams} from "react-router-dom";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 
 //url에 있는 값을 반환해주는 함수
 function Detail(){
     const {id} = useParams();
+    const [loading, setLoading] = useState(true);
     const getMovie = async () => {
         const json = await(
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-            ).json();
+            ).json();  
+
+            console.log(json.data.movie);
+
+        setMovie(json.data.movie);
+        setLoading(false);
     };
+    const [movie, setMovie] = useState([]);
+   
+
+
     useEffect( () => {
         getMovie();
     }, []);
-    return <h1>Detail</h1>
+    
+    return (
+    <div>
+        {loading ? <h1>Loading...</h1> : 
+           <div>
+               <Link to ={`/`}>back</Link>
+                <h1>{movie.title_long}</h1>
+                <img src={movie.large_cover_image} />
+                <div>Download: {movie.download_count} Like: {movie.like_count} rating: {movie.rating}</div>
+                <div>{movie.genres}</div>
+                <div>{movie.description_intro}</div>
+                <a href= {movie.url}>{movie.url}</a>
+            </div>
+        }
+    </div>
+    );
 }
 
 export default Detail;
