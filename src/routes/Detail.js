75c52e4b41b227bb2dae6ce27b34d726853097eb
wang-hsoo <dhkd4873@naver.com>
2022-03-03 01:19:23 +0900

    commit

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -12,7 +12,7 @@ function Detail(){
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
             ).json();  
 
-            console.log(json.data.movie);
+        
 
         setMovie(json.data.movie);
         setLoading(false);
@@ -31,14 +31,19 @@ function Detail(){
         <div className={styles.loader}>
           <span>Loading...</span>
         </div> : 
-           <div>
+           <div className={styles.detailCon}>
                <Link to ={`/`}>back</Link>
                 <h1>{movie.title_long}</h1>
-                <img src={movie.large_cover_image} />
-                <div>Download: {movie.download_count} Like: {movie.like_count} rating: {movie.rating}</div>
-                <div>{movie.genres}</div>
-                <div>{movie.description_intro}</div>
-                <a href= {movie.url}>{movie.url}</a>
+                
+                <div className={styles.detailMain}>
+                    <img src={movie.large_cover_image} />
+                    <div>
+                        <div>Download: {movie.download_count} Like: {movie.like_count} rating: {movie.rating}</div>
+                        <div>{movie.genres}</div>
+                        <div>{movie.description_intro}</div>
+                        <a href= {movie.url}>{movie.url}</a>
+                    </div>
+                </div>
             </div>
         }
     </div>
